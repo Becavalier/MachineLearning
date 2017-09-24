dbb8aa7f0f8b2cb6007b64f98db93fdd1229ebaf
Jason <yhorg@hotmail.com>
2017-09-24 23:52:45 +0800

    Update notes of NBC.

--- KNN/README.md
+++ KNN/README.md
@@ -4,4 +4,4 @@
 ![KNN](https://raw.githubusercontent.com/Becavalier/MachineLearning/master/KNN/knn-normal.png)
 
 ### 加权型
-![KNN](https://raw.githubusercontent.com/Becavalier/MachineLearning/master/KNN/knn-mean.png)
+![KNN](https://raw.githubusercontent.com/Becavalier/MachineLearning/master/KNN/knn.png)
---
+++ NBC/nbc.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# 基于概率论的贝叶斯定理，适用于标称型（离散）数据；
+
+# 基于贝努利模型；
+
+def loadDataSet():
+    postingList= [
+        ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+        ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+        ['my', 'dalmatian', 'is', 'so', 'cute', 'I', 'love', 'him'],
+        ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+        ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+        ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']
+    ]
+    # 类别向量, 1-bad, 0-normal；
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        # 并集；
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    # 与词汇表等长；
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print('Missing word: %s' % word)
+    return returnVec
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs) # 0.5
+    # 防止乘积均为0的情况；
+    p0Num = np.ones(numWords)
+    p1Num = np.ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        # bad；
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 取对数避免下溢出；
+    p1Vect = np.log(p1Num/p1Denom)
+    p0Vect = np.log(p0Num/p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+
+    p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+
+    testEntry = ['love', 'my', 'dalmatian']
+    print(setOfWords2Vec(myVocabList, testEntry))
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(classifyNB(thisDoc, p0V, p1V, pAb))
